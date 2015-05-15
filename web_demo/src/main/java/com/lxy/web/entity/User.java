e87c6932d275eb8aaa4c85ba4d3edd427283cfe2
hylclxy <hylclxy@foxmail.com>
2015-05-15 18:18:12 +0800

    d

--- web_demo/src/main/java/com/lxy/web/entity/User.java
+++ web_demo/src/main/java/com/lxy/web/entity/User.java
@@ -6,9 +6,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="sys_user")
+@XmlRootElement
 public class User {
 	
 	@Id
